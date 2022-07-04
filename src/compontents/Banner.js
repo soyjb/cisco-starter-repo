79d56153baf18cf929754139ef2a740e2596e772
soyjb <thejulianbeltran@gmail.com>
2022-07-04 01:58:53 -0400

    added title and h1

---
+++ src/compontents/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      <div>
+        <title>Cisco Sextant</title>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/compontents/Exhibit.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Exhibit = () => {
+  return (
+    <div>
+        <div>
+            <h1>Cisco Sextant</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum iure, cumque vero vel odio, ab quia totam consequatur perferendis aspernatur nulla, deserunt necessitatibus ut omnis? Nesciunt fugit et iusto corrupti!</p>
+        </div>
+    </div>
+  )
+}
+
+export default Exhibit
